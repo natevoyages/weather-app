d6b3fc7be84e19df5eb7d303f65d53a6d692c1e9
NateVoyages <allanromo20@gmail.com>
2023-06-27 21:15:53 -0700

    Add weather property processor funct

--- javascript.js
+++ javascript.js
@@ -4,5 +4,13 @@ async function showLondonWeather() {
     const response = await fetch('https://api.weatherapi.com/v1/current.json?key=caa55f97010a475a877172806232706&q=london', {mode: 'cors'});
     const weatherData = await response.json();
     console.log(weatherData);
+    processWeather(weatherData);
 }
+
+function processWeather(weatherData){
+   const tempF = weatherData.current.temp_f;
+   const tempC =  weatherData.current.temp_c;
+   console.log("F:", tempF, "C:", tempC);
+}
+
 showLondonWeather();
